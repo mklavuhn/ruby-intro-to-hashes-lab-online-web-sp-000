875a49a11af09e0c72f34b330aa83d7686a1b3b8
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-24 20:26:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,9 +57,10 @@ def monopoly_with_fourth_tier
 	    names: {
 	      reading_railroad: {
 	        'mortgage_value': '$100'
+	      },
+	      pennsylvania_railroad: {
 	        
 	      },
-	      pennsylvania_railroad: {},
 	      b_and_o_railroad: {},
 	      shortline: {}
 	    },
