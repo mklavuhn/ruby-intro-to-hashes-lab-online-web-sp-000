3ae82381cea217745d50c941d042229e73a507fc
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-24 20:06:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,9 +21,9 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
-	    pieces: "4"
-	    name: {}
-	    rent_in_dollars: {}
+	    :pieces "4"
+	    :name {}
+	    :rent_in_dollars {}
 	  } 
 	}
 
